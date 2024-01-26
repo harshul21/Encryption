fc58176cc019fab78357422814b2c6e7214af945
harshul <harshulgarg21@gmail.com>
2024-01-26 23:14:45 +0530

    new working changes

--- index1.js
+++ index1.js
@@ -7,8 +7,7 @@ function encrypt(plainText, securitykey, outputEncoding) {
     const cipher = crypto.
         createCipheriv("aes-128-ecb", securitykey, null);
     return Buffer.
-        concat([cipher.update(plainText), cipher.final()]).
-        toString(outputEncoding);
+        concat([cipher.update(plainText), cipher.final()])
 }
 
 //AES decryption
@@ -16,8 +15,7 @@ function decrypt(cipherText, securitykey, outputEncoding) {
     const cipher = crypto.
         createDecipheriv("aes-128-ecb", securitykey, null);
     return Buffer.
-        concat([cipher.update(cipherText), cipher.final()]).
-        toString(outputEncoding);
+        concat([cipher.update(cipherText), cipher.final()])
 }
 
 
@@ -30,10 +28,11 @@ const secretMessage = "This is a secret message";
 //AES encryption
 const encrypted =
     encrypt(secretMessage, securitykey, "base64");
-console.log("Encrypted message:", encrypted);
-console.log("security key:", securitykey.toString('hex'));
+console.log("Encrypted message:", encrypted.toString('hex'));
+console.log("security key:", securitykey);
+
 
 //AES decryption
 const decrypted = decrypt(Buffer.
     from(encrypted, "base64"), securitykey, "utf8")
-console.log("Decrypted string:", decrypted);
+console.log("Decrypted string:", decrypted.toString('utf8'));
--- index3.js
+++ index3.js
@@ -1,18 +1,17 @@
 const CryptoJS = require('crypto-js');
 
-var ciphertext = "79a247e48ac27ed33ca3f1919067fa64";
-var key = "6268890F-9B58-484C-8CDC-34F9C6A9";
-var iv = "6268890F-9B58-48";
+var ciphertext = "34d142432a778d1301619ffaf47d17740a4b459f44bb58fc5a83dd3259ec41e7";
+var key = "3dbd28c01c70b33a9bd686f24ea4521a";
+var iv = "0000000000000000"; // 16 bytes IV
 
 var ciphertextWA = CryptoJS.enc.Hex.parse(ciphertext);
-var keyWA = CryptoJS.enc.Utf8.parse(key);
-var ivWA = CryptoJS.enc.Utf8.parse(iv);
-var ciphertextCP = { ciphertext: ciphertextWA };
+var keyWA = CryptoJS.enc.Hex.parse(key);
+var ivWA = CryptoJS.enc.Hex.parse(iv);
 
 var decrypted = CryptoJS.AES.decrypt(
-    ciphertextCP,
+    { ciphertext: ciphertextWA },
     keyWA, 
     { iv: ivWA }
 );
 
-console.log(decrypted.toString(CryptoJS.enc.Utf8)); // Apple
+console.log(decrypted.toString(CryptoJS.enc.Utf8)); 
