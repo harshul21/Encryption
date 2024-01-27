4e6785125f842b9d43bdebf6d7f2374c691658ab
harshul <harshulgarg21@gmail.com>
2024-01-27 14:02:23 +0530

    file reading added

--- index4.js
+++ index4.js
@@ -1,18 +1,16 @@
 const crypto = require('crypto');
+const fs = require('fs');
 
 let iv = Buffer.alloc(16).fill(0);
 
-
+// let key = '12345678123456781234567812345678';
+// varHexMesssage = '24f64656f9e32bcf50759ebbfc2cd674'
+// let secret_message = ':)';
 // aes 256-bit cipher block chaining (cbc) encryption/decryption
-let secret_message = ':)';
-let key = '12345678123456781234567812345678';
 
-// let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-// let encrypted = cipher.update(secret_message, 'utf-8', 'hex'); converting utf-8 content to hex
-// encrypted += cipher.final('hex');
-//console.log('encrypted: ' + encrypted)
+const key = fs.readFileSync('key.txt', 'utf-8').trim(); // Assuming key is stored in 'key.txt'
+const varHexMesssage = fs.readFileSync('message.txt', 'utf-8').trim(); // Assuming message is stored in 'message.txt'
 
-varHexMesssage = '24f64656f9e32bcf50759ebbfc2cd674'
 let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
 let decrypted = decipher.update(varHexMesssage, 'hex', 'utf-8'); //converting hex to utf-8
 decrypted += decipher.final('utf-8');
